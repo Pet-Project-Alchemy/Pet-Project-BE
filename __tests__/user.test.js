faa9a8960b28dadc746b261a7a8f0bffc846850b
Caleb Pendergraft <cackmed@gmail.com>
2020-02-20 15:57:46 -0800

    auth tests passing

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -64,8 +64,54 @@ describe('user routes', () => {
 
     return request(app)
       .post('/api/v1/auth/login')
-      .send({ email: 'badEmail@notgood.io', password: 'password'
+      .send({ email: 'badEmail@notgood.io', password: 'password' })
+      .then(res => {
+        expect(res.status).toEqual(401);
+        expect(res.body).toEqual({
+          status: 401,
+          message: 'Invalid Email or Password'
+        });
+      });
+  });
+
+  it('fails to login a user with a bad password', async() => {
+    await User.create({
+      email: 'test@test.com',
+      password: 'password'
     });
-    
-  })
+
+    return request(app)
+      .post('/api/v1/auth/login')
+      .send({ email: 'test@test.com', password: 'notright' })
+      .then(res => {
+        expect(res.status).toEqual(401);
+        expect(res.body).toEqual({
+          status: 401,
+          message: 'Invalid Email or Password'
+        });
+      });
+  });
+
+  it('can verify if a user is logged in', async() => {
+    const user = await User.create({
+      email: 'test@test.com',
+      password: 'password'
+    });
+
+    const agent = request.agent(app);
+
+    await agent
+      .post('/api/v1/auth/login')
+      .send({ email: 'test@test.com', password: 'password' });
+
+    return agent
+      .get('/api/v1/auth/verify')
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: user.id,
+          email: 'test@test.com',
+          __v: 0
+        });
+      }) ;
+  });
 });
