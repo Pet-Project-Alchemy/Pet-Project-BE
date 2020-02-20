54981f6c31879e137ecc3de4e0487ad80330c12c
Nikki <nikki.satopdx@gmail.com>
2020-02-20 09:51:21 -0800

    mobbing initail commit

---
+++ __tests__/app.test.js
@@ -0,0 +1,24 @@
+// require('dotenv').config();
+
+// const request = require('supertest');
+// const app = require('../lib/app');
+// const connect = require('../lib/utils/connect');
+// const mongoose = require('mongoose');
+
+// describe('app routes', () => {
+//   beforeAll(() => {
+//     connect();
+//   });
+
+//   beforeEach(() => {
+//     return mongoose.connection.dropDatabase();
+//   });
+
+//   afterAll(() => {
+//     return mongoose.connection.close();
+//   });
+// });
+
+it('to equal true', () => {
+  expect(true).toEqual(true);
+});
