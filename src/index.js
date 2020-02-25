284c91728d45e380de2b6484017601f1fce45cc7
samSterns <sam.g.sterns@gmail.com>
2020-02-24 17:28:18 -0800

    cleaned up files and moved stuff to FE

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// moved functionality of this file to app.js
+// moved functionality of this file to FE Chat.js
 
 const path = require('path');
 const http = require('http');
@@ -13,8 +13,56 @@ const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
-const { addUser, removeUser, getUser } = require('./utils/users'); 
+
 app.use(express.static(publicDirectoryPath));
+
+// const users = [];
+
+// const addUser = ({ id, username, room }) => {
+//   username = username.trim().toLowerCase();
+//   room = room.trim().toLowerCase();
+//   if(!username || !room) {
+//     return {
+//       error: 'Username and room are required!'
+//     };
+//   }
+//   const existingUser = users.find(() => {
+//     return (username.room === room && username.username === username);
+//   });
+//   if(existingUser) {
+//     return {
+//       error: 'Username is in use!'
+//     };
+//   }
+
+//   const user = { id, username, room };
+//   users.push(user);
+//   return { user };
+// };
+
+// const removeUser = (id) => {
+//   const index = users.findIndex((user) => user.id === id);
+//   if(index !== -1) {
+//     return users.splice(index, 1)[0];
+//   }
+// };
+
+// const getUser = (id) => {
+//   return users.find((user) => user.id === id);
+// };
+
+// const getUsersInRoom = (room) => {
+//   return users.filter((user) => user.room === room);
+// };
+
+// module.exports = {
+//   addUser,
+//   removeUser,
+//   getUser,
+//   getUsersInRoom
+// };
+
+
 
 io.on('connection', (socket) => {
   socket.on('hi', (data) => {
