177d814c0a98776f9c5f6d9bf3a36f9720b9ffaa
cackmed <53550131+cackmed@users.noreply.github.com>
2020-02-26 16:38:55 -0800

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('./lib/utils/connect')();
 
 const app = require('./lib/app');
 // Backend port change for testing
-const PORT = process.env.PORT || 7891;
+const PORT = process.env.PORT || 7890;
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
